3040322133d2194bb060e709c0348c1c1915d209
Eric Wagner <git@alphamule.com>
2018-11-09 09:17:56 -0800

    initial commit

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Foo {
+    public static void main(String args[]) {
+        System.out.println(hello("eric"));
+    }
+
+    public String hello(String name) {
+        return "Hello, " + name;
+    }
+}
